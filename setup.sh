0ddf6a6c2b3dc9c143bb4bd764c4267e2f473efa
ItzKernel <therealkernelpanic@gmail.com>
2023-03-04 00:23:49 +0000

    better setup script :D

--- setup.sh
+++ setup.sh
@@ -2,44 +2,74 @@
 
 DATE=$(date '+%y%m%d%H%M%S%N')
 CONFIGDIR=$HOME/.config
-DOTS=(eww fish hypr kitty neofetch nvim)
+declare -A PATHS=( ["config"]="$CONFIGDIR" ["dot"]="$HOME" )
 SCRIPTPATH="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 BACKUPDIR=$CONFIGDIR/dotbackup$DATE
 # Log prefix
 PRE="\e[35m#\e[0m"
 
+# Telling the user what's going to happen and asking them if they want to continue
+# or not
 echo "This script will replace your existing configuration with the dotfiles in this repository."
 echo "Before that, it will make a backup of your existing dotfiles to \"${BACKUPDIR}\""
 echo "Do you want to continue? (y)"
 read contvar
 
+# If the user has definitely accepted the stuff then we can continue, we exit otherwise
 if [[ $contvar != "y" ]] && [[ $contvar != "" ]]; then
     exit 0
 fi
 
+echo ""
 echo -e "$PRE The script is running from $SCRIPTPATH"
+echo ""
 
-echo -e "$PRE Making backup..."
-
-echo -e "$PRE$PRE Creating backup directory"
+# Making a backup directory where we can put the old dotfiles
+echo -e "$PRE Creating backup directory"
 mkdir $BACKUPDIR
 
-echo -e "$PRE$PRE$PRE Putting stuff inside the backup dir"
-for dir in $CONFIGDIR/*; do
-    BASENAME=$(basename $dir)
+# Making the script be able to interact with hidden files
+shopt -s dotglob
 
-    # If the dots array contains the directory
-    if [[ " ${DOTS[*]} " =~ " $BASENAME " ]]; then
-        echo -e "$PRE$PRE$PRE Backing up ${BASENAME}..."
-        mv ${dir} ${BACKUPDIR}
-    fi
+# Looping through the dot path types
+for TYP in "${!PATHS[@]}"; do
+    # Making a subdir in the backup folder so the backup is organized
+    mkdir $BACKUPDIR/$TYP
+    echo -e "$PRE ($TYP) Starting the replacing process..."
+
+    # Looping through the dotfiles in this repo
+    for REPODOTPATH in ${SCRIPTPATH}/${TYP}/*; do
+        REPODOTNAME=$(basename $REPODOTPATH)
+
+        # This shit goes berserk if i dont check this:
+        #
+        # It tries moving every directory into the backup folder
+        # because the basename is "*" (and this is not very cool)
+        if [[ $REPODOTNAME == *\* ]]; then
+            echo -e "$PRE$PRE ($TYP) Dir empty, skipping..."
+            break
+        fi
+
+        # Looping through the local dotfiles
+        for LOCALDOTPATH in ${PATHS[$TYP]}/*; do
+            LOCALDOTNAME=$(basename $LOCALDOTPATH)
+
+            # If there's a local dotfile that can be replaced by the ones
+            # located in the repo, we back it up to the newly created backup dir
+            if [[ $LOCALDOTNAME == $REPODOTNAME ]]; then
+                echo -e "$PRE$PRE ($TYP) Found local $LOCALDOTNAME, backing up..."
+                mv $LOCALDOTPATH $BACKUPDIR/$TYP # > /dev/null 2>&1
+            fi
+        done
+
+        # We make a symlink to the repo's dotfiles
+        echo -e "$PRE$PRE ($TYP) Linking $REPODOTNAME..."
+        ln -s $SCRIPTPATH/$TYP/$REPODOTNAME ${PATHS[$TYP]}/$REPODOTNAME
+    done
 done
 
-echo -e "$PRE Linking directories..."
-for dir in "${DOTS[@]}"; do
-    echo -e "$PRE$PRE Linking ${dir}"
-    ln -s $SCRIPTPATH/$dir $CONFIGDIR/$dir
-done
-
+echo ""
 echo "Setup done! If you haven't encountered any errors then it's probably successful too."
-
+echo "Note: Please do not mess with the dotfiles in this repository if you don't know what you're doing."
+echo "If you want to remove the dotfiles, remove the symlinks first in your local directories"
+echo ""
